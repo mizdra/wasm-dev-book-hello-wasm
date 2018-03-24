9705e7cce86f22d8a1259b418555781e2c39a81a
mizdra <pp.mizdra@gmail.com>
2018-03-24 20:42:34 +0900

    chore: update example

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+extern crate tinymt;
 extern {
     fn date_now() -> f64;
 }
+
+use tinymt::tinymt32;
 
 #[no_mangle]
 pub fn get_timestamp() -> f64 {
@@ -10,7 +13,18 @@ pub fn get_timestamp() -> f64 {
 }
 
 #[no_mangle]
-pub fn increment(num: i32) -> i32 {
-    num + 1
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[no_mangle]
+pub fn rand() -> u32 {
+    let param = tinymt32::Param {
+        mat1: 0x8F7011EE,
+        mat2: 0xFC78FF1F,
+        tmat: 0x3793fdff,
+    };
+    let seed = 1;
+    tinymt32::from_seed(param, seed).gen()
 }
 
